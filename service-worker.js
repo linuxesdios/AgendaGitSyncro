3963880f5326f0631c354feefb8de542d93a1d88
linuxesdios <linuxesdios@gmail.com>
2025-11-29 18:42:47 +0100

    modified:   agenda.html 	modified:   agendaphone.html 	new file:   assets/icons/generate-icons.html 	modified:   manifest.json 	modified:   manifest_movil.json 	new file:   service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'agenda-pablo-v1';
+const urlsToCache = [
+  './agenda.html',
+  './agendaphone.html',
+  './assets/css/agenda.css',
+  './assets/css/agendaphone.css',
+  './assets/js/supabase-sync.js',
+  './assets/js/bottom-nav.js',
+  './assets/js/calendario.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
